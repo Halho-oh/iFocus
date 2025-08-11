8666bd23876b80c629c2da0de1ee1cbb2020adee
Halho-oh <kuronagajyu.h415@gmail.com>
2025-08-11 20:14:28 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Service Worker登録
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
-}
-
-document.getElementById('startBtn').addEventListener('click', () => {
-    alert('Focus Mode Started!');
-});
